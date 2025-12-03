22d45ac49ffdde7df595d990003b6fdf2f848068
Naykiry <vladkrut.90@mail.ru>
2025-12-03 20:51:44 +0300

    test

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = "dev-secret"
     SECRET_KEY = "dev-secrrrrrrrrret"
+    SECRET_KEY = "dev-secrrrrrrrrret"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "warehouse.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
